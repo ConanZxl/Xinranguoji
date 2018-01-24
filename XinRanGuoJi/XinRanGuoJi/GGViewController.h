5bc632ee0ec62e884d02ac91c8f6bf6541b7c338
272094436 <272094436@qq.com>
2018-01-24 10:13:18 +0800

    添加一个类

---
+++ XinRanGuoJi/XinRanGuoJi/GGViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGViewController.h
+//  XinRanGuoJi
+//
+//  Created by 鼎尚 on 2018/1/24.
+//  Copyright © 2018年 XPLSolte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGViewController : UIViewController
+
+@end
---
+++ XinRanGuoJi/XinRanGuoJi/GGViewController.m
@@ -0,0 +1,38 @@
+//
+//  GGViewController.m
+//  XinRanGuoJi
+//
+//  Created by 鼎尚 on 2018/1/24.
+//  Copyright © 2018年 XPLSolte. All rights reserved.
+//
+
+#import "GGViewController.h"
+
+@interface GGViewController ()
+
+@end
+
+@implementation GGViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor redColor];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
