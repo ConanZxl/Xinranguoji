a1da92a2d7088e1faf1b98e0f16f2251166014e4
272094436 <272094436@qq.com>
2018-01-24 10:14:23 +0800

    添加   MM类

---
+++ XinRanGuoJi/XinRanGuoJi/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  XinRanGuoJi
+//
+//  Created by 鼎尚 on 2018/1/24.
+//  Copyright © 2018年 XPLSolte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
---
+++ XinRanGuoJi/XinRanGuoJi/MMViewController.m
@@ -0,0 +1,38 @@
+//
+//  MMViewController.m
+//  XinRanGuoJi
+//
+//  Created by 鼎尚 on 2018/1/24.
+//  Copyright © 2018年 XPLSolte. All rights reserved.
+//
+
+#import "MMViewController.h"
+
+@interface MMViewController ()
+
+@end
+
+@implementation MMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor=[UIColor redColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
